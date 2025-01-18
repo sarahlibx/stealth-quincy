74d6c2c2fad87f73f6b10692e487d12f648d4bd6
Sarah Smith <155038730+sarahlibx@users.noreply.github.com>
2025-01-18 11:47:26 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const fans = document.querySelector(".fans");
+const fish = document.querySelector(".fish");
+const pets = document.querySelector(".pets");
+const toggle = document.querySelector("#toggle");
+const ball = document.querySelector(".ball");
+const h1 = document.querySelector("h1");
+const body = document.querySelector("body");
+
+fans.addEventListener("mouseover", function () {
+  fans.innerText = "123K";
+});
+
+fish.addEventListener("mouseover", function () {
+  fish.innerText = "47K";
+});
+
+pets.addEventListener("mouseover", function () {
+  pets.innerText = "20K";
+});
+
+toggle.addEventListener("click", function () {
+  if (body.classList.contains("light")) {
+    body.classList.remove("light");
+    ball.classList.remove("move-right");
+    h1.innerText = "Stealth Quincy";
+  } else {
+    body.classList.add("light");
+    ball.classList.add("move-right");
+    h1.innerText = "Party Quincy";
+  }
+});
